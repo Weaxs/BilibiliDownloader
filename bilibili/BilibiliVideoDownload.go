47a897b1b8c2078920e85793874213234033247c
WeaxsZhang <zhangykm@yonyou.com>
2020-08-22 16:34:58 +0800

    cids

--- bilibili/BilibiliVideoDownload.go
+++ bilibili/BilibiliVideoDownload.go
@@ -5,11 +5,12 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
-	"reflect"
-	"strings"
+	"strconv"
 )
 
-func getAidFromBvid(bvId string) string {
+// 通过BVID获取AID和CID
+// 同一个视频的bvid和aid是相同的，cid是分P的，没P的cid不同
+func getAidFromBvid(bvId string) (aid int64, cids []int64, bvid string) {
 	url := "https://api.bilibili.com/x/web-interface/view?bvid=" + bvId
 
 	//req, err := http.NewRequest(http.MethodGet, url, nil)
@@ -25,35 +26,43 @@ func getAidFromBvid(bvId string) string {
 
 	resp, err := http.Get(url)
 
-	if err != nil || resp == nil {
-		return ""
+	if err != nil || resp == nil || resp.StatusCode != 200 {
+		return -1, nil, bvId
 	}
 	body, err := ioutil.ReadAll(resp.Body)
-	if body == nil || err != nil {
-		return ""
+	if err != nil {
+		return -1, nil, bvid
 	}
 	respMessage := make(map[string]interface{})
 	err = json.Unmarshal(body, &respMessage)
 	if err != nil {
-		return ""
+		return -1, nil, bvid
 	}
-	body, err = json.Marshal(respMessage["data"])
+	tmp, err := json.Marshal(respMessage["data"])
 	data := make(map[string]interface{})
-	err = json.Unmarshal(body, &data)
+	err = json.Unmarshal(tmp, &data)
 	if err != nil {
-		return ""
+		return -1, nil, bvid
 	}
-	reflect.TypeOf(data["aid"])
+	tmp, err = json.Marshal(data["pages"])
+	var pages []map[string]interface{}
+	err = json.Unmarshal(tmp, &pages)
 
-	return fmt.Sprint()
+	var cids []int64
+	for i, page := range pages {
+		fmt.Print("P" + strconv.Itoa(i))
+		cids = append(cids, page["cid"].(int64))
+	}
+
+	return -1, cids, bvid
 
 }
 
 func main() {
 	fmt.Print("")
-	aid := "BV1x54y1e7zf"
-	if strings.HasPrefix(aid, "BV") {
-		getAidFromBvid(aid)
-	}
+	bvid := "BV1ap4y197tR"
+	aid, cid, bvid := getAidFromBvid(bvid)
+
+	print("aid=" + strconv.FormatInt(aid, 10) + "   cid=" + fmt.Sprint(cid))
 
 }
